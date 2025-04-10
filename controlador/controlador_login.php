46bad32eced716d8daf56fc963bccdc12bcfd28e
Sack09 <salwilcer@gmail.com>
2025-04-10 15:42:02 -0600

    Implementación de Autentificación
    Se implementa con la autentificación  con google.

--- controlador/controlador_login.php
+++ controlador/controlador_login.php
@@ -1,5 +1,58 @@
 <?php
-if (!empty($_POST["btningresar"])) {
+require __DIR__ . '/../vendor/autoload.php';
+$config = require 'google_config.php';
+
+$client = new Google_Client();
+$client->setClientId($config['client_id']);
+$client->setClientSecret($config['client_secret']);
+$client->setRedirectUri($config['redirect_uri']);
+$client->addScope("email");
+$client->addScope("profile");
+
+if (isset($_GET['code'])) {
+    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    $client->setAccessToken($token);
+
+    $google_service = new Google_Service_Oauth2($client);
+    $google_user = $google_service->userinfo->get();
+
+    $email = $google_user->email;
+    $nombre = $google_user->givenName;
+    $apellido = $google_user->familyName;
+
+    $stmt = $db_connection->prepare("SELECT * FROM usuario WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        // Usuario ya existe, iniciar sesión
+        $datos = $result->fetch_object();
+        $_SESSION["idusuario"] = $datos->idusuario;
+        $_SESSION["nombre"] = $datos->nombre;
+        $_SESSION["apellidoP"] = $datos->apellidoP;
+        $_SESSION["rol"] = $datos->rol;
+
+        header("Location: data/index.php");
+        exit();
+    } else {
+        // Registrar un nuevo usuario utilizando los datos de Google
+        $stmt = $db_connection->prepare("INSERT INTO usuario (email, nombre, apellidoP, rol, activo, `delete`) VALUES (?, ?, ?, '2', 1, 1)");
+        $stmt->bind_param("sss", $email, $nombre, $apellido);
+
+        if ($stmt->execute()) {
+            $_SESSION["idusuario"] = $db_connection->insert_id;
+            $_SESSION["nombre"] = $nombre;
+            $_SESSION["apellidoP"] = $apellido;
+            $_SESSION["rol"] = 'Usuario';
+
+            header("Location: data/index.php");
+            exit();
+        } else {
+            echo '<div class="error-msg"><i class="fa fa-times-circle"></i> Error al registrar el usuario.</div>';
+        }
+    }
+} elseif (!empty($_POST["btningresar"])) {
     if (!empty($_POST["email"]) && !empty($_POST["password"])) {
         $email = $_POST["email"];
         $password = $_POST["password"];
--- controlador/google_config.php
+++ controlador/google_config.php
@@ -2,6 +2,6 @@
 return [
     'client_id' => '103163134576-ctlb2v91irniva2vllsco1646tsanp73.apps.googleusercontent.com',
     'client_secret' => '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',
-    'redirect_uri' => 'http://localhost/17deoctubre/', // Cambia por tu dominio en producción.
+    'redirect_uri' => 'http://localhost/17deoctubre/',
 ];
 ?>
